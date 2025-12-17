9bcafaa602a95bd0f4feffdb7a7346c9315d18fc
majuac <castromaju57@gmail.com>
2025-12-17 14:37:01 -0300

    utilizando finally e using

--- ByteBank/OperacaoFinanceiraException.cs
+++ ByteBank/OperacaoFinanceiraException.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace ByteBank
 {
-    internal class OperacaoFinanceiraException
+    public class OperacaoFinanceiraException : Exception
     {
+        public OperacaoFinanceiraException()
+        {
+
+        }
+
+        public OperacaoFinanceiraException(string mensagem)
+            : base(mensagem)
+        {
+
+        }
+
+        public OperacaoFinanceiraException(string mensagem, Exception excecaoInterna) 
+            : base (mensagem, excecaoInterna)
+        {
+
+        }
     }
 }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,21 +13,64 @@ namespace ByteBank
         {
             try
             {
+                CarregarContas();
+            }
+            catch
+            {
+                Console.WriteLine("CATCH NO METODO MAIN");
+            }
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
+            Console.WriteLine();
+        }
+
+        private static void CarregarContas()
+        {
+            using(LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+            // --------------------------------------------------
+
+
+            //LeitorDeArquivo leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contas.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //} 
+            //finally
+            //{
+            //    Console.WriteLine("Executando o finally");
+            //    if(leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
+
+        }
+
+        private static void TestaInnerException()
+        {
+            try
+            {
                 ContaCorrente conta1 = new ContaCorrente(4521, 456845);
                 ContaCorrente conta2 = new ContaCorrente(4532, 154654);
 
                 conta1.Transferir(10000, conta2);
-            }catch (Exception e)
+            }
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
 
-                Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
-
-                Console.WriteLine(e.InnerException.Message);
-                Console.WriteLine(e.InnerException.StackTrace);
             }
         }
+
         private static void Metodo()
         {
             TestaDivisao(0);
